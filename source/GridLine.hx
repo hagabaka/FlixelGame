afc3e51c0d58e9c1670df33434da0906f123851b
Yaohan Chen <yaohan.chen@gmail.com>
2016-04-05 12:00:10 -0400

    Remove the need for GridLineParameters

--- source/GridLine.hx
+++ source/GridLine.hx
@@ -1,57 +1,40 @@
 package;
 
 import Lambda;
-enum GridLineParameters {
-	Horizontal(row:Int, columns:IntIterator);
-	Vertical(column:Int, rows:IntIterator);
-    Diagonal(slope:Int, startRow:Int, columns:IntIterator);
-	Invalid;
-}
+
 class GridLine
 {
 	var containedPositions:Array<GridPosition> = new Array();
 
 	public function new(from:GridPosition, to:GridPosition) 
 	{
-		var parameters:GridLineParameters = GridLineParameters.Invalid;
 		if (from.column == to.column) {
-			parameters = GridLineParameters.Vertical(from.column, range(from.row, to.row));
+            for (row in range(from.row, to.row)) {
+                containedPositions.push(new GridPosition(from.column, row));
+            }
 		} else if (from.row == to.row) {
-			parameters = GridLineParameters.Horizontal(from.row, range(from.column, to.column));
-        } else if (from.column - to.column == from.row - to.row) {
-            parameters = GridLineParameters.Diagonal(
-                1,
-                from.column > to.column ? to.row : from.row,
-                range(from.column, to .column)
-            );
-        } else if (from.column - to.column == to.row - from.row) {
-            parameters = GridLineParameters.Diagonal(
-                -1,
-                from.column > to.column ? to.row : from.row,
-                range(from.column, to .column)
-            );
-        }
-
-		switch(parameters) {
-			case GridLineParameters.Vertical(column, rows):
-				for (row in rows) {
-					containedPositions.push(new GridPosition(column, row));
-				}
-			case GridLineParameters.Horizontal(row, columns):
-				for (column in columns) {
-					containedPositions.push(new GridPosition(column, row));
-				}
-            case GridLineParameters.Diagonal(slope, startRow, columns):
+            for (column in range(from.column, to.column)) {
+                containedPositions.push(new GridPosition(column, from.row));
+            }
+        } else {
+            var startRow = from.column > to.column ? to.row : from.row;
+            var slope = null;
+            if (from.column - to.column == from.row - to.row) {
+                slope = 1;
+            } else if (from.column - to.column == to.row - from.row) {
+                slope = -1;
+            }
+            if (slope != null) {
                 var distance = 0;
-				for (column in columns) {
+                for (column in range(from.column, to.column)) {
                     containedPositions.push(new GridPosition(
                     column,
                     startRow + slope * distance
                     ));
                     distance++;
-				}
-			case GridLineParameters.Invalid:
-		}
+                }
+            }
+        }
 	}
 
     public function contains(position:GridPosition):Bool {
